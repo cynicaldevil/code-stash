3876a8d3dfefd3097f23e80c2c1cf0f2e502d82a
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-18 01:36:15 +0530

    Added controller for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+	def create
+		@snippet = Snippet.find(params[:snippet_id])
+		@comment  = @snippet.comments.create(comment_params)
+		redirect_to snippet_path(@snippet)
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:commenter, :body)
+	end
+end
