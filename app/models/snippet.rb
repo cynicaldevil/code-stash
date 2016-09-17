e21599a3e73bbdfacf33c983fb630fd791f1cd4f
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-18 01:37:03 +0530

    Added routes for comments

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,4 @@
 class Snippet < ApplicationRecord
+	has_many :comments
 	validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-    resources :snippets
+    resources :snippets do
+        resources :comments
+    end
 
     get 'home/index'
 
