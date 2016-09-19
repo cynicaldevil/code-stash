a1bda8b40c28ef7d2f9c77d5843e3d9b83e9feeb
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-19 16:13:53 +0530

    Added view and controller method to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
 		redirect_to snippet_path(@snippet)
 	end
 
+	def destroy
+		@snippet = Snippet.find(params[:snippet_id])
+		@comment = @snippet.comments.find(params[:id])
+		@comment.destroy
+		redirect_to snippet_path(@snippet)
+	end
+
 	private
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
