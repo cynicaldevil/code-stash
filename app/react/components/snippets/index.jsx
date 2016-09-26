00a7bcc78094f2f51af591a81c6ceb7f8e627a27
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-26 19:58:12 +0000

    Added additional styles to index

--- app/react/components/snippets/index.jsx
+++ app/react/components/snippets/index.jsx
@@ -29,28 +29,38 @@ class Snippet extends React.Component {
         const styles = {
             code: {
                 width: 900,
-                border: '1px solid black',
+                border: '1px solid #CDCDCD',
                 margin: 10,
                 overflow: 'hidden',
-                height: count >= 10 ? 154 : ((count+1) * 15) + 8,
-                zIndex: 100,
+                height: count >= 10 ? 157: ((count+1) * 15) + 8,
                 cursor: 'pointer'
             },
             link: {
                 display: 'block',
-                textDecoration: 'none'
+                textDecoration: 'none',
+                width: 900
             },
             title: {
                 fontFamily: `'Roboto Condensed', sans-serif`,
-                fontSize: 17
+                fontSize: 19,
+                letterSpacing: -0.5,
+                color: '#001C57'
             },
             main: {
-                margin: 20
+                margin: 30
+            },
+            bar: {
+                height: 2,
+                width: 1000,
+                background: 'linear-gradient(to right, #6294FF , white)',
+                marginBottom: 20,
+                marginTop: 5
             }
         };
         return (
             <div style={styles.main}>
                 <div style={styles.title}>{snippet.title}</div>
+                <div style={styles.bar} />
                 <a style={styles.link} href="https://github.com">
                     <div style={styles.code}>
                         <CodeMirror value={snippet.text} options={options} />
