524757557c2a4a889dd2b58630cad083d8e7abd4
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-26 19:39:34 +0000

    Extracted snippets' code and created a new Component for it

--- app/react/components/snippets/index.jsx
+++ app/react/components/snippets/index.jsx
@@ -5,6 +5,62 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import CodeMirror from 'react-codemirror';
 require('../../../../node_modules/codemirror/lib/codemirror.css');
 
+class Snippet extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+
+        const snippet = this.props.snippet;
+
+        const options = {
+            lineNumbers: true,
+            readOnly: true,
+        };
+
+        const code = snippet.text;
+        let i = 0, count = 0;
+        while(count < 10 && i < code.length) {
+            if(code.charAt(i) === '\n')
+                count++;
+            i++;
+        }
+        const styles = {
+            code: {
+                width: 900,
+                border: '1px solid black',
+                margin: 10,
+                overflow: 'hidden',
+                height: count >= 10 ? 154 : ((count+1) * 15) + 8,
+                zIndex: 100,
+                cursor: 'pointer'
+            },
+            link: {
+                display: 'block',
+                textDecoration: 'none'
+            },
+            title: {
+                fontFamily: `'Roboto Condensed', sans-serif`,
+                fontSize: 17
+            },
+            main: {
+                margin: 20
+            }
+        };
+        return (
+            <div style={styles.main}>
+                <div style={styles.title}>{snippet.title}</div>
+                <a style={styles.link} href="https://github.com">
+                    <div style={styles.code}>
+                        <CodeMirror value={snippet.text} options={options} />
+                    </div>
+                </a>
+            </div>
+        );
+    }
+}
+
 class Snippets extends React.Component {
     constructor(props) {
         super(props);
@@ -15,56 +71,18 @@ class Snippets extends React.Component {
     }
 
     render() {
-        const options = {
-            lineNumbers: true,
-            readOnly: true,
-        };
 
-        const styles = {
-            codemirror: {
-                width: 900,
-                border: '1px solid black',
-                margin: 10,
-                overflow: 'hidden'
-            }
-        };
-
-        const snippets = this.props.snippets.map((snippet, index) => {
-
-            const code = snippet.text;
-            let i = 0, count = 0;
-            while(count < 10 && i < code.length) {
-                if(code.charAt(i) === '\n')
-                    count++;
-                i++;
         const styles = {
             header: {
                 fontFamily: `'Roboto Condensed', sans-serif`,
                 fontWeight: 400,
                 letterSpacing: -1.7,
             }
-            const innerStyles = {
-                code: {
-                    ...styles.codemirror,
-                    height: count >= 10 ? 154 : ((count+1) * 15) + 8,
-                    zIndex: 100,
-                    cursor: 'pointer'
-                },
-                link: {
-                    display: 'block',
-                    textDecoration: 'none'
-                }
-            };
+        };
 
-            return (
-                <div key={index}>
-                    <div>{snippet.title}</div>
-                    <a style={innerStyles.link} href="https://github.com">
-                        <div style={innerStyles.code}>
-                            <CodeMirror value={snippet.text} options={options} />
-                        </div>
-                    </a>
-                </div>)
+        const snippets = this.props.snippets.map((snippet, index) => {
+
+            return <Snippet key={index} snippet={snippet} />;
         });
         return (
             <MuiThemeProvider>
