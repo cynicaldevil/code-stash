0abc945021ae55fff81746109637ada8d59c791d
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-09-28 19:19:07 +0000

    Added more styles to buttons in show snippet component

--- app/react/components/snippets/show.jsx
+++ app/react/components/snippets/show.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ShowComment from '../comments/show';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import CodeMirror from 'react-codemirror';
 require('../../../../node_modules/codemirror/lib/codemirror.css');
 require('../../../assets/stylesheets/codemirror-extend.css');
@@ -35,13 +35,20 @@ class ShowSnippet extends React.Component {
                 cursor: 'pointer'
             },
             title: {
-                fontFamily: `'Roboto Condensed', sans-serif`,
-                width: 900,
-                fontSize: 19,
-                letterSpacing: -0.5,
-                color: '#001C57',
-                display: 'flex',
-                justifyContent: 'space-between',
+                main: {
+                    fontFamily: `'Roboto Condensed', sans-serif`,
+                    width: 1000,
+                    fontSize: 19,
+                    letterSpacing: -0.5,
+                    color: '#001C57',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                },
+                buttons: {
+                    // border: '1px inset #B8B8B8',
+                    marginRight: 5,
+                    width: 75
+                }
             },
             main: {
                 margin: 30
@@ -55,23 +62,30 @@ class ShowSnippet extends React.Component {
             },
             comments: {
                 border: '1px solid black'
-            }
+            },
         };
 
         return (
             <MuiThemeProvider>
             <div style={styles.main}>
                 <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400" rel="stylesheet" />
-                <div style={styles.title}>
+                <div style={styles.title.main}>
                     {snippet.title}
+                    <div style={{display: 'flex'}}>
+                        <div style={styles.title.buttons}>
+                            <FlatButton label='Edit' href={'/snippets/' + snippet.id + '/edit'}/>
+                        </div>
+                        <div style={styles.title.buttons}>
+                            <FlatButton label='Back' href={'/snippets'}/>
+                        </div>
+                    </div>
                 </div>
                 <div style={styles.bar} />
 
                 <div style={styles.code}>
                     <CodeMirror value={snippet.text} options={options} />
                 </div>
-                <RaisedButton label='Edit' href={'/snippets/' + snippet.id + '/edit'}/>
-                <RaisedButton label='Back' href={'/snippets'}/>
+
 
                 <div style={styles.comments}>
                     {comments}
